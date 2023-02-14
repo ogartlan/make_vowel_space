656906eab6a6e234794c938be54985919b6c3a29
Oisín <102963904+ogartlan@users.noreply.github.com>
2023-02-14 04:07:18 +0000

    from_scratch
    Trying to plot a scatter plot of averages per word utterance from scratch

--- Plot_vowel_space.R
+++ Plot_vowel_space.R
@@ -67,66 +67,98 @@ exclude_these_Vs <-
 #================================================================#
 # if you recorded multiple versions of the same vowel,
 # then average over them here
-df_sum <- df %>%
-    # Filters out vowels taht are in the "excluded" list
-    dplyr::filter(!vowel %in% exclude_these_Vs) %>% 
-    # group rows by column values in the DataFrame. 
-    # Because of the %>% above, the data frame is
-    # the filtered csv
-    group_by(vowel, IPA, time_index) %>%
-    summarise(F1 = mean(F1, na.rm = TRUE), # na.rm skips over any NA values
-              F2 = mean(F2, na.rm = TRUE),
-              F3 = mean(F3, na.rm = TRUE)) %>%
-    group_by(vowel, IPA) %>%
-    # create a 5-sample rolling average
-    # mutate is for adding new variables
-    mutate(F1s = zoo::rollmean(F1, 5, na.pad = TRUE), # na.pad adds the NAs back in to fit the original data frame.
-           F2s = zoo::rollmean(F2, 5, na.pad = TRUE),
-           F3s = zoo::rollmean(F3, 5, na.pad = TRUE))
+#   MY OWN ATTEMPT AT GETTING AN AVERAGE FOR EACH UTTERANCE
+#df_avgs <- df %>%
+# number of rows in the data frame
+num_rows <- nrow(df) - 1
+
+#making a vector (array) of length = number of rows / 10
+F1_avgs <- numeric(num_rows/10)
+F2_avgs <- numeric(num_rows/10)
+
+F1_current <- 0
+F2_current <- 0
+
+q <- 0
+while (q <= num_rows/10){
+    for (i in 1:10){
+        F1_current <- F1_current + df[i + q, "F1"]
+        F2_current <- F2_current + df[i + q, "F2"]
+        }
+    F1_avgs[q+1] <- F1_current / 10
+    F2_avgs[q+1] <- F2_current / 10
+    F1_current <- 0
+    F2_current <- 0
+    q <- q + 1
+}
+
+#plot(F1_avgs, F2_avgs)
+
+df1 <- data.frame(x = F1_avgs, y = F2_avgs)
+ggplot(df1, aes(x, y)) + geom_point()
+
+
+#================================================================#
+#df_sum <- df %>%
+#    # Filters out vowels taht are in the "excluded" list
+#    dplyr::filter(!vowel %in% exclude_these_Vs) %>% 
+#   # group rows by column values in the DataFrame. 
+#    # Because of the %>% above, the data frame is
+#    # the filtered csv
+#    group_by(vowel, IPA, time_index) %>%
+#    summarise(F1 = mean(F1, na.rm = TRUE), # na.rm skips over any NA values
+#              F2 = mean(F2, na.rm = TRUE),
+#              F3 = mean(F3, na.rm = TRUE)) %>%
+#    group_by(vowel, IPA) %>%
+#    # create a 5-sample rolling average
+#    # mutate is for adding new variables
+#    mutate(F1s = zoo::rollmean(F1, 5, na.pad = TRUE), # na.pad adds the NAs back in to fit the original data frame.
+#           F2s = zoo::rollmean(F2, 5, na.pad = TRUE),
+#           F3s = zoo::rollmean(F3, 5, na.pad = TRUE))
 
 #================================================================#
 # Make another data frame of just the trajectory endpoints
 # **In the original code, time_index == 9 in all cases below. 
 # ** Since I am only using this for simple vowels, not diphthongs,
 # ** I will grab it from the middle of the vowel, i.e. time 5.
-df_endpt <- df_sum %>%
-    dplyr::filter(!vowel %in% exclude_these_Vs) %>%
-    group_by(vowel, IPA) %>%
-    summarise(
-        # F1s is ....
-        F1s = F1s[time_index == 5],
-        F2s = F2s[time_index == 5],
-        F3s = F3s[time_index == 5],
-        #
-        # F1 is ....
-        F1 = F1[time_index == 5],
-        F2 = F2[time_index == 5],
-        F3 = F3[time_index == 5])
-
+#df_endpt <- df_sum %>%
+#    dplyr::filter(!vowel %in% exclude_these_Vs) %>%
+#    group_by(vowel, IPA) %>%
+#    summarise(
+#        # F1s is ....
+#        F1s = F1s[time_index == 5],
+#        F2s = F2s[time_index == 5],
+#        F3s = F3s[time_index == 5],
+#        #
+#        # F1 is ....
+#        F1 = F1[time_index == 5],
+#        F2 = F2[time_index == 5],
+#        F3 = F3[time_index == 5])
+#
 #================================================================#
 # Plot it!
-px_v_space_smooth <- df_sum %>%
-    dplyr::filter(time_index > 1, time_index < 10) %>%
-    ggplot(.)+
-    aes(x = F2s, y = F1s, group = vowel, color = vowel)+
-    # trajectory ** commented out for now
-    #geom_path(linewidth = 1.1)+
-    # IPA symbol encapsulated in a label
-    geom_label(data = df_endpt,label.padding = unit(0.2, "line"), 
-               aes(label = IPA))+
-    # another layer of text for the IPA symbol
-    # to ensure that it is visible,
-    # but still shows a trace of the underlying color
-    geom_text(data = df_endpt,
-              aes(label = IPA),
-              color = "black", alpha = 0.5)+
-    scale_x_reverse(position = "top", name = "F2 (Hz)")+
-    scale_y_reverse(position = "right", name = "F1 (Hz)")+
-    theme_bw()+
-    theme(legend.position = "none")
-px_v_space_smooth
+#px_v_space_smooth <- df_sum %>%
+#    dplyr::filter(time_index > 1, time_index < 10) %>%
+#    ggplot(.)+
+#    aes(x = F2, y = F1, group = vowel, color = vowel)+
+#    # trajectory ** commented out for now
+#    #geom_path(linewidth = 1.1)+
+#    # IPA symbol encapsulated in a label
+#    geom_label(data = df_endpt,label.padding = unit(0.2, "line"), 
+#               aes(label = IPA))+
+#    # another layer of text for the IPA symbol
+#    # to ensure that it is visible,
+#    # but still shows a trace of the underlying color
+#    geom_text(data = df_endpt,
+#              aes(label = IPA),
+#              color = "black", alpha = 0.5)+
+#    scale_x_reverse(position = "top", name = "F2 (Hz)")+
+#    scale_y_reverse(position = "right", name = "F1 (Hz)")+
+#    theme_bw()+
+#    theme(legend.position = "none")
+#px_v_space_smooth
 
 # Save the plot
-ggsave(px_v_space_smooth, file = "My_vowel_no_diph.png",
-       height = 3.7, width = 4.8, dpi = 600)
+#ggsave(px_v_space_smooth, file = "My_vowel_no_diph.png",
+#       height = 3.7, width = 4.8, dpi = 600)
 # End!
